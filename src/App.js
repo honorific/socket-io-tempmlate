9e426b9dc448aca6661f8da517dbecf995e31539
honorific <honorific.info@gmail.com>
2023-09-03 10:24:59 +0330

    setup boilerplate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 function App() {
-  return <div className='App'></div>
+  const sendMessage = () => {}
+
+  return (
+    <div className='App'>
+      <input type='text' placeholder='message...' />
+      <button onClick={sendMessage}>Send</button>
+    </div>
+  )
 }
 
 export default App
