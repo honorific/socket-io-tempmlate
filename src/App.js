8ed5567fdb90934ee7b57d1df859b1d2fa0f0333
honorific <honorific.info@gmail.com>
2023-09-03 11:20:00 +0330

    testing the emit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-function App() {
-  const sendMessage = () => {}
+import {useEffect} from 'react'
+import {io} from 'socket.io-client'
+const socket = io.connect('http://localhost:3001')
 
+function App() {
+  const sendMessage = () => {
+    socket.emit('send-message', {message: 'hello'})
+  }
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      alert(data.message)
+    })
+  }, [socket])
   return (
     <div className='App'>
       <input type='text' placeholder='message...' />
