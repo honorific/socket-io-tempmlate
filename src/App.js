205d6200b87e8935128d9eb30cb0d77a2e1c7a20
honorific <honorific.info@gmail.com>
2023-09-03 11:43:00 +0330

    show message in span

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import {io} from 'socket.io-client'
 const socket = io.connect('http://localhost:3001')
 
 function App() {
+  const [message, setMessage] = useState('')
+  const [messageReceived, setMessageReceived] = useState('')
   const sendMessage = () => {
-    socket.emit('send-message', {message: 'hello'})
+    socket.emit('send-message', {message})
   }
 
   useEffect(() => {
-    socket.on("receive_message", (data) => {
-      alert(data.message)
+    socket.on('receive_message', (data) => {
+      setMessageReceived(data.message)
     })
   }, [socket])
   return (
     <div className='App'>
-      <input type='text' placeholder='message...' />
+      <input
+        type='text'
+        placeholder='message...'
+        onChange={(e) => setMessage(e.target.value)}
+      />
       <button onClick={sendMessage}>Send</button>
+      <span>
+        <b>Message:</b>
+      </span>
+      <span>{messageReceived}</span>
     </div>
   )
 }
